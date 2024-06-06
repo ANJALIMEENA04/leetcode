0d2451fe6d4e303e7607f8e5aac1379b43226877
ANJALIMEENA04 <115338203+ANJALIMEENA04@users.noreply.github.com>
2024-06-07 04:29:44 +0530

    Time: 22 ms (27.45%), Space: 21 MB (24.15%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    // void func(int i,int k,vector<int>& candidates,vector<vector<int>>& vec,vector<int> ds)
+    // {
+    //     if(i==candidates.size()){
+    //         if(k==0) vec.push_back(ds);
+    //         return;
+    //     }
+    //     else{
+    //         if(candidates[i]<=k){
+    //             ds.push_back(candidates[i]);
+    //             func(i,k-candidates[i],candidates,vec,ds);
+    //             ds.pop_back();
+    //         }
+    //         func(i+1,k,candidates,vec,ds);
+    //     }
+    // }
+
+    void f(int i,vector<int>ds,int sum,vector<int>& arr,vector<vector<int>>& vec){
+        if(sum==0) {
+            vec.push_back(ds);
+            return;
+        }
+        if(i>=arr.size() || sum<0 || arr[i]>sum) return;
+        ds.push_back(arr[i]);
+        f(i,ds,sum-arr[i],arr,vec);
+        ds.pop_back();
+        f(i+1,ds,sum,arr,vec);
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+         vector<vector<int>> vec;
+         vector<int> ds;
+         sort(candidates.begin(),candidates.end());
+        //  func(0,target,candidates,vec,ds);
+         f(0,ds,target,candidates,vec);
+         return vec;
+    }
+};
