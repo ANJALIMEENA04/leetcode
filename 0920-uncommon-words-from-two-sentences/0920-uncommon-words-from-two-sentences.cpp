53bab5b5d8069d36391d247a73089f824295221e
ANJALIMEENA04 <115338203+ANJALIMEENA04@users.noreply.github.com>
2024-09-17 22:43:00 +0530

    Time: 2 ms (51.8%), Space: 8.9 MB (45.08%) - LeetHub

--- 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.cpp
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
     vector<string> uncommonFromSentences(string s1, string s2) {
-        vector<string> ans,v1,v2;
-        map<string,int>m;
-        string t = "";
-        for(int i=0;i<s1.size();i++){
-            if(s1[i]==' ') {
-                v1.push_back(t);
-                t="";
+        vector<string> ans;
+        unordered_map<string,int> mp;
+        string word="";
+        for(auto x: s1){
+            if(x==' ')  {
+                mp[word]++;
+                word="";
             }
-            else t += s1[i];
-            if(i==s1.size()-1){
-                v1.push_back(t);
-                t="";
-            }
+            else word+=x;
         }
-        for(int i=0;i<s2.size();i++){
-            if(s2[i]==' ') {
-                v2.push_back(t);
-                t="";
+        mp[word]++;
+        word="";
+        for(auto x: s2) {
+            if(x==' ')  {
+                mp[word]++;
+                word="";
             }
-            else t += s2[i];
-            if(i==s2.size()-1){
-                v2.push_back(t);
-                t="";
-            }
+            else word+=x;
         }
-        for(auto it:v1) ++m[it];
-        for(auto it:v2) ++m[it];
-        for(auto it:m) if(it.second==1) ans.push_back(it.first);
-        return ans;  
+        mp[word]++;
+        for(auto x: mp){
+            if(x.second==1) ans.push_back(x.first);
+        }
+        return ans;
     }
+
 };
